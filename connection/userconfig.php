2325891fab04bcbc59c4644834825d06b9ea6bb6
Mohaimin Alif <imohaiminalif@gmail.com>
2023-09-08 20:20:25 +0600

    working on user_config

--- connection/userconfig.php
+++ connection/userconfig.php
@@ -1,94 +1,112 @@
-
 <?php
+ $servername = "localhost";
+ $username = "root";
+ $password = "";
+ $dbname = "lbsp";
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "lbsp";
+ 
+ $conn=mysqli_connect($servername,$username,$password,$dbname);
+ 
+ if(!$conn){
+     die(mysqli_error($conn));
+ }
+ 
 
-$conn=mysqli_connect($servername,$username,$password,$dbname);
-
-if(!$conn){
-    die(mysqli_error($con));
+ 
+ 
+ if($_SERVER['REQUEST_METHOD']=='POST'){
+     
+     $user_type=$_POST['user_type'];
+     $id=$_POST['id'];
+     $pass=$_POST['pass'];
+ 
+ 
+     // if($user_type =='admin_t' && $id == 'admin' && $pass == '1111'){
+ 
+             
+     //     header("Location: ../employee/dashboard.php");
+ 
+     // }
+ 
+//      if($user_type=='client_t'){ $query ="SELECT * FROM client_t WHERE client_code = '$id' AND pass = '$pass'";}
+//      if($user_type=='relationshipmanager_t'){$query = "SELECT * FROM relationshipmanager_t WHERE manager_id = '$id' AND pass = '$pass'";}
+//      if($user_type=='headofsettlement_t'){$query = "SELECT * FROM headofsettlement_t WHERE head_id = '$id' AND pass = '$pass'";}
+//      else{$query ="SELECT * FROM admin_t WHERE admin_ id = '$id' AND pass = '$pass'";}
+     
+//  // $query = "SELECT * FROM client_t WHERE client_code = '$id' AND pass = '$pass'";
+ 
+//  $res =  $conn->query("SELECT * FROM client_t WHERE client_code = '$id' AND pass = '$pass'");
+ 
+$result = $conn->query("SELECT * FROM client_t WHERE client_code = '$id' AND pass = '$pass'");
+if ($result) {
+    while ($row = $result->fetch_assoc()) {
+        // Process the retrieved data here
+    }
+    $result->free_result(); // Free the result set when done
+} else {
+    // Handle query execution failure
+    echo "Query failed: " . $conn->error;
 }
 
+ 
+    //  $res = $conn->query($query)->fetch_assoc();
+ 
+ 
+ 
 
-
-if($_SERVER['REQUEST_METHOD']=='POST'){
-    
-    $user_type=$_POST['user_type'];
-    $id=$_POST['id'];
-    $pass=$_POST['pass'];
-
-
-    if($user_type =='admin_t' && $id == 'admin' && $pass == '1111'){
-
-            
-        header("Location: ../employee/dashboard.php");
-
-    }
-
-    if($user_type=='client_t'){ $query ="SELECT * FROM client_t WHERE client_code = '$id' AND pass = '$pass'";}
-    if($user_type=='relationshipmanager_t'){$query = "SELECT * FROM relationshipmanager_t WHERE manager_id = '$id' AND pass = '$pass'";}
-    if($user_type=='headofsettlement_t'){$query = "SELECT * FROM headofsettlement_t WHERE head_id = '$id' AND pass = '$pass'";}
-    
-    
-if($query != null){
-    echo $query;
-};
-
-    $res = $conn->query($query)->fetch_assoc();
-
-
-
-    if($res == null){
-      $invalid=1;
-      
-    }else{
-
-        session_start();
-        
-        if($user_type =='client_t'){
-
-          $_SESSION = $res;
-          $_SESSION['client_code'] = $res['client_code'];
-          $_SESSION['role'] = "client";
-          
-          
-          header("Location: ../user/dashboard.php");
-
-        }
-
-        if($user_type =='headofsettlement_t'){
-            $_SESSION['client_code'] = $res['client_code'];
-            $_SESSION['name'] = $res['name'];
+ 
+         session_start();
+         
+         if($user_type =='client_t'){
+       
+            $_SESSION = $row;
             $_SESSION['role'] = "client";
+       
+            
+            header("Location: ../user/dashboard.php");
+  
+          }
+  
+          if($user_type =='headofsettlement_t'){
+            $_SESSION = $res;
+            // $_SESSION['client_code'] = $res['client_code'];
+            // $_SESSION['name'] = $res['name'];
+            // $_SESSION['role'] = "client";
             
             header("Location: ../employee/hosDash.html");
   
         }
 
         if($user_type =='relationshipmanager_t'){
-            $_SESSION['client_code'] = $res['client_code'];
-            $_SESSION['name'] = $res['name'];
-            $_SESSION['role'] = "client";
+
+            $_SESSION = $res;
+            // $_SESSION['client_code'] = $res['client_code'];
+            // $_SESSION['name'] = $res['name'];
+            // $_SESSION['role'] = "client";
             
-            header("Location: ../employee/hosDash.html");
+            header("Location: ../employee/managerDash.php");
   
         }
 
+        if($user_type =='relationshipmanager_t'){
+
+            $_SESSION = $res;            
+            header("Location: ../employee/dashboard.php");
+  
+        }
+ 
+         
+ 
         
-
-       
-    
-    }
-
-
-   }
-
-
-
-
+     
+     }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 
 ?>
 
